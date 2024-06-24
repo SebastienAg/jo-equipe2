d3014e3494cf4bb133f6b5dd464e3d893439bd09
Sébastien AGNEZ [42f361e1-1927-4887-9439-3d9acad675cc] <Sebastien.AGNEZ@ca-ps.com>
2024-06-24 15:18:52 +0200

    feat: ajout tableau des médailles

--- src/app/medals-table/medals-table.component.ts
+++ src/app/medals-table/medals-table.component.ts
@@ -1,45 +1,119 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as Papa from 'papaparse';
 
 @Component({
   selector: 'app-medals-table',
   templateUrl: './medals-table.component.html',
   styleUrls: ['./medals-table.component.css']
 })
-export class MedalsTableComponent implements OnInit {
+export class MedalsTableComponent {
   medals = new Map<string, {gold: number, silver: number, bronze: number}>();
-  remainingTime: number = 180;
-  intervalId: any;
 
-  ngOnInit(): void {
-    this.medals.set('USA', {gold: 39, silver: 41, bronze: 33});
-    this.medals.set('China', {gold: 38, silver: 32, bronze: 18});
-    // add more countries...
+  constructor() {
+    Promise.all([
+      this.getMedalsByCountry('assets/Athle_sample.csv'),
+      this.getMedalsByCountry('assets/Nat_50m_sample.csv')
+    ]).then(([res1, res2]) => {
+      this.medals = new Map([...res1, ...res2]);
+    }).catch((error) => {
+      console.error(error);
+    });
   }
 
-  startCountdown(seconds: number) {
-    this.remainingTime = seconds;
+  //This method acquires a csv file with the following columns: rank, athlete name, birthdate, country, mark, venue, event
+  //and returns a list of medals by country.
+  // Method to parse CSV file and return list of medals by country
+  getMedalsByCountry(filePath: string): Promise<Map<string, {gold: number, silver: number, bronze: number}>> {
+    return new Promise((resolve, reject) => {
+      Papa.parse(filePath, {
+        download: true,
+        header: true,
+        complete: (results: { data: any; }) => {
+          let medalsByCountry = new Map<string, {gold: number, silver: number, bronze: number}>();
 
-    this.intervalId = setInterval(() => {
-      this.remainingTime--;
-      if (this.remainingTime <= 0) {
-        clearInterval(this.intervalId);
-      }
-    }, 1000);
+          for (let row of results.data) {
+            if(row !== '') {
+              let country;
+
+              // @ts-ignore
+
+              if(row['NAT.'] && typeof row['NAT.'] === 'string') {
+                // @ts-ignore
+                if(row['NAT.'].length == 3){
+                  // @ts-ignore
+                  country = this.convertCountryToIsoCode(row['NAT.']);
+                } else {
+                  // @ts-ignore
+                  country = row['NAT.'];
+                }
+              } else {
+                // @ts-ignore
+                console.error('Invalid value for NAT.: ', row['NAT.']);
+              }
+
+              // @ts-ignore
+              let medalType = row['PLACE'];
+              if (country && medalType && !medalsByCountry.has(country)) {
+                medalsByCountry.set(country, {gold: 0, silver: 0, bronze: 0});
+              }
+              if (country && medalType) {
+                this.getMedalType(medalType, medalsByCountry, country);
+              }
+            }
+          }
+          resolve(medalsByCountry);
+        },
+        error: (error: any) => {
+          reject(error);
+        }
+      });
+    });
   }
 
-  public start(): void {
-    this.startCountdown(3 * 60);
+  //implement a method that converts the name of a country to it's ISO code, like 'Brazil' to 'BRA'.
+// Mapping of country names to ISO codes
+  private countryToIsoCode = new Map<string, string>([
+    ['ITA', 'Italie'],
+    ['NED', 'Pays-Bas'],
+    ['FIN', 'Finlande'],
+    ['BRA', 'Brésil'],
+    ['UKR', 'Ukraine'],
+    ['GBR', 'Royaume-Uni'],
+    ['ZIM', 'Zimbabwe'],
+    ['CIV', 'Côte d\'Ivoire'],
+    ['GRE', 'Grèce'],
+    ['CAN', 'Canada'],
+    ['USA', 'États-Unis'],
+    ['SGP', 'Singapour'],
+    ['POR', 'Portugal'],
+    ['BOT', 'Botswana'],
+    ['SWE', 'Suède'],
+    ['QAT', 'Qatar'],
+    ['NZL', 'Nouvelle-Zélande'],
+    ['AUT', 'Autriche'],
+    ['BUL', 'Bulgarie']
+    // ... continue this list with all countries and their ISO codes
+  ]);
+
+  // Method to convert country name to ISO code
+  convertCountryToIsoCode(countryName: string): string {
+    return this.countryToIsoCode.get(countryName) || 'Unknown';
   }
 
-  public stop(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
+  private getMedalType(medalType: string, medalsByCountry: Map<string, {
+    gold: number;
+    silver: number;
+    bronze: number
+  }>, country: string) {
+    if (medalType.startsWith('1.') ) { // Assuming '1' represents Gold
+      // @ts-ignore
+      medalsByCountry.get(country).gold += 1;
+    } else if (medalType.startsWith('2.')) { // Assuming '2' represents Silver
+      // @ts-ignore
+      medalsByCountry.get(country).silver += 1;
+    } else if (medalType.startsWith('3.')) { // Assuming '3' represents Bronze
+      // @ts-ignore
+      medalsByCountry.get(country).bronze += 1;
     }
-  }
-
-  getFormattedTime(): string {
-    const minutes = Math.floor(this.remainingTime / 60);
-    const seconds = this.remainingTime % 60;
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 }
