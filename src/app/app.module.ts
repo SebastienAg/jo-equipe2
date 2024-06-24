6186f5b824cf1d814b59e230a98cb7f932dfde22
Sébastien AGNEZ [42f361e1-1927-4887-9439-3d9acad675cc] <Sebastien.AGNEZ@ca-ps.com>
2024-06-24 12:38:37 +0200

    feat: ajout tableau des médailles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { MedalsTableComponent } from './medals-table/medals-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    MedalsTableComponent
   ],
   imports: [
     BrowserModule,
